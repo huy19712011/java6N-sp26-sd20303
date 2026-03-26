5f5be892dc85220c1a43e2feef5df2e00b878109
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-03-26 10:27:34 +0700

    Security basics: Spring boot auto config for security

--- src/main/java/org/example/java6nsp26sd20303/entity/Book.java
+++ src/main/java/org/example/java6nsp26sd20303/entity/Book.java
@@ -1,15 +1,13 @@
 package org.example.java6nsp26sd20303.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "books")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
