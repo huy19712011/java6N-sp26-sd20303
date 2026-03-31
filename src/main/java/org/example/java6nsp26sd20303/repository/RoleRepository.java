ced8b5f3a4e7e60e810545bb82c07b4eabcaf94d
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-03-31 10:59:40 +0700

    Security: Using database (User - Role) - part 1

---
+++ src/main/java/org/example/java6nsp26sd20303/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.example.java6nsp26sd20303.repository;
+
+import org.example.java6nsp26sd20303.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
