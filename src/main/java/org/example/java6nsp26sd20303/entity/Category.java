1c125b6b67201aef33f88eca19d39bc92b750002
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-04-14 10:32:45 +0700

    JPA: Product and Category

---
+++ src/main/java/org/example/java6nsp26sd20303/entity/Category.java
@@ -0,0 +1,54 @@
+package org.example.java6nsp26sd20303.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        product.setCategory(this);
+        this.products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+
+        this.products.remove(product);
+        product.setCategory(null);
+    }
+}
