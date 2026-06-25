4aba60f1af68157c049c330575fdf4415979f4a6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-06-25 15:30:50 +0700

    Product API: fix mistake - adding ProductRepository

---
+++ src/main/java/org/example/java6nsp26sd20303/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.example.java6nsp26sd20303.repository;
+
+import org.example.java6nsp26sd20303.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
